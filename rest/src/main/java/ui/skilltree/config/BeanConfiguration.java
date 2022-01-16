2d87b19a0c4e29ba3e74105db75d6d52174f5150
florianhartung <florianhartung@users.noreply.github.com>
2022-01-16 23:54:27 +0100

    Spring resource loading (specifically 'skilltree.json') for production mode

--- rest/src/main/java/ui/skilltree/config/BeanConfiguration.java
+++ rest/src/main/java/ui/skilltree/config/BeanConfiguration.java
@@ -1,40 +1,44 @@
 package ui.skilltree.config;
 
 import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ResourceUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.EncodedResource;
 
-import java.io.*;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class BeanConfiguration {
 
-    private static final String PATH = "classpath:skilltree.json";
+    /**
+     * Sources of the skilltree.json resource ordered by priority
+     */
+    private final List<Resource> skillTreeResources = List.of(new FileSystemResource("./config/skilltree.json"), new ClassPathResource("META-INF/skilltree.json"));
 
     @Bean("skillTreeConfiguration")
     public SkillTreeConfiguration get() {
-        try {
-            File file = ResourceUtils.getFile(PATH);
-            String json = readFromFile(file);
-            return parseSkillTreeConfiguration(json);
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (Resource resource : skillTreeResources) {
+            try {
+                return read(resource);
+            } catch (IOException ignored) {
+            }
         }
         return null;
     }
 
-    private String readFromFile(File file) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-        StringBuilder result = new StringBuilder();
-        String line;
-        while((line = bufferedReader.readLine()) != null) {
-            result.append(line);
-        }
-        return result.toString();
-    }
+    private SkillTreeConfiguration read(Resource resource) throws IOException {
+        EncodedResource encodedResource = new EncodedResource(resource, StandardCharsets.UTF_8);
 
-    private SkillTreeConfiguration parseSkillTreeConfiguration(String json) {
-        return new Gson().fromJson(json, SkillTreeConfiguration.class);
+        JsonReader reader = new JsonReader(encodedResource.getReader());
+
+        return new Gson().fromJson(reader, SkillTreeConfiguration.class);
     }
 }
